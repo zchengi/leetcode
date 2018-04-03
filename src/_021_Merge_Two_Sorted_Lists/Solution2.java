b2a4ce17d3b89b0714e2ef1a3a4bc15e8e941ce8
cheng <792702352@qq.com>
2018-04-03 23:26:15 +0800

    LeetCode 021. Merge Two Sorted Lists

--- src/_021_Merge_Two_Sorted_Lists/Solution2.java
+++ src/_021_Merge_Two_Sorted_Lists/Solution2.java
@@ -1,9 +1,9 @@
-package _21_Merge_Two_Sorted_Lists;
+package _021_Merge_Two_Sorted_Lists;
 
 import util.ListNode;
 
 /**
- * LeetCode 21. Merge Two Sorted Lists
+ * LeetCode 021. Merge Two Sorted Lists
  * <p>
  * 归并排序的 merge 思想
  * <p>
@@ -11,21 +11,15 @@ import util.ListNode;
  * 空间复杂度：O(1)
  *
  * @author cheng
- *         2018/4/3 22:46
+ *         2018/4/3 22:58
  */
-public class Solution1 {
+public class Solution2 {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 
         ListNode dummyNode = new ListNode(0);
         ListNode cur = dummyNode;
-        while (l1 != null || l2 != null) {
-            if (l1 == null) {
-                cur.next = l2;
-                l2 = l2.next;
-            } else if (l2 == null) {
-                cur.next = l1;
-                l1 = l1.next;
-            } else if (l1.val <= l2.val) {
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
                 cur.next = l1;
                 l1 = l1.next;
             } else {
@@ -33,6 +27,13 @@ public class Solution1 {
                 l2 = l2.next;
             }
             cur = cur.next;
+        }
+
+        if (l1 != null) {
+            cur.next = l1;
+        }
+        if (l2 != null) {
+            cur.next = l2;
         }
 
         return dummyNode.next;
@@ -43,6 +44,6 @@ public class Solution1 {
         int[] arr2 = {1, 3, 4};
         ListNode l1 = new ListNode(arr1);
         ListNode l2 = new ListNode(arr2);
-        System.out.println(new Solution1().mergeTwoLists(l1, l2));
+        System.out.println(new Solution2().mergeTwoLists(l1, l2));
     }
 }
