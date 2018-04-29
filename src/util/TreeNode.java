028623373a78943084c8b7bc0692fda4565e3c0c
cheng <792702352@qq.com>
2018-04-29 22:45:31 +0800

    add TreeNode class

---
+++ src/util/TreeNode.java
@@ -0,0 +1,20 @@
+package util;
+
+/**
+ * Definition for a binary tree node.
+ *
+ * @author cheng
+ *         2018/4/29 22:21
+ */
+
+public class TreeNode {
+
+    public  int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+    }
+
+}
