6189cca604724c15706e8a06fe7ffa69eeeee07b
cheng <792702352@qq.com>
2018-07-10 13:35:51 +0800

    Test Sort

---
+++ MyDemo/TestSort/test.java
@@ -0,0 +1,34 @@
+package TestSort;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * @author cheng
+ *         2018/7/10 13:10
+ */
+public class test {
+
+    private static void test1(Integer[] nums) {
+        long start = System.currentTimeMillis();
+        Arrays.sort(nums, (i1, i2) -> i2 - i1);
+        System.out.println(System.currentTimeMillis() - start);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    private static void test2(Integer[] nums) {
+        long start = System.currentTimeMillis();
+        Arrays.sort(nums);
+        Collections.reverse(Arrays.asList(nums));
+        System.out.println(System.currentTimeMillis() - start);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void main(String[] args) {
+
+        Integer[] nums = {15, 20, 9, 6};
+        Integer[] nums2 = {15, 20, 9, 6};
+        test1(nums);
+        test2(nums2);
+    }
+}
